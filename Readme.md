8cdd3ca055ab47a053b13015a86bec03b61378c1
陈佳志 <jiazhi.chen@vdin.net>
2018-01-30 11:43:48 +0800

    增加配置文件启动方式

--- Readme.md
+++ Readme.md
@@ -1,1 +1,29 @@
 在命令行中使用jdbc
+
+# 1. 编译
+## 因为oracle jdbc没有在maven库，需要从官网下载后安装oracle jdbc jar到本地maven库
+```
+mvn install:install-file -Dfile=D:/localjar/ojdbc8-12.2.0.1.0.jar -DgroupId=oracle -DartifactId=oracle -Dversion=12 -Dpackaging=jar
+```
+如果不想自己编译，也可以直接使用target目录下的 jdbc-cli-1.0.jar
+
+# 2.使用方式1
+
+配置文件
+```properties
+db1.type=oracle
+db1.url=
+db1.username=
+db1.password=
+
+```
+db1 为配置id ,一个配置文件可以有多个id,一次会话中只能读取一个id
+
+type可以设置oracle/o,mysql/m,postgresql/p
+
+url 为jdbc url
+username 为用户名
+password 为密码
+
+# 3.使用方式2
+使用命令行参数
--- src/main/java/top/fastsql/jdbccli/MainApp.java
+++ src/main/java/top/fastsql/jdbccli/MainApp.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import java.io.File;
 import java.io.FileInputStream;
 import java.sql.Driver;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -20,60 +21,60 @@ public class MainApp {
     private static String url = null;
     private static String userName = null;
     private static String password = null;
+    private static DbType dbType = null;
 
     private static JdbcTemplate jdbcTemplate = null;
 
+
+    private enum DbType {
+        ORACLE, MY_SQL, POSTGRESQL;
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.print("Welcome jdbc-cli......");
-        System.out.print("Mode1-use properties file; Mode2-input. Input 1/2 (2) :");
+        System.out.println("Welcome jdbc-cli......");
+        System.out.print("Select Mode ~ Mode 1-Use properties file; Mode 2-Input Parameters. Input 1/2 (2) :");
         String mode = new Scanner(System.in).nextLine().trim();
         if ("1".equals(mode)) {
             System.out.print("Input Config File Path (./config.properties) :");
             String file = new Scanner(System.in).nextLine().trim();
+            if (file.equals("")) {
+                file = "./config.properties";
+            }
             Properties properties = new Properties();
             properties.load(new FileInputStream(new File(file)));
 
-            System.out.print("Input Config File Id (db1) :");
-
-        } else if ("2".equals(mode)) {
+            System.out.print("Input Config  Id (db1) :");
+            String id = new Scanner(System.in).next();
+            String type = (String) properties.get(id + "." + "type");
+            boolean success = setDriverByDbType(type);
+            if (!success) {
+                System.exit(0);
+            }
+            setUrl((String) properties.get(id + "." + "url"));
+            userName = (String) properties.get(id + "." + "username");
+            password = (String) properties.get(id + "." + "password");
+        } else {
             String db;
             while (true) {
                 System.out.print("Input DataBase Type o-Oracle/m-Mysql/p-Postgresql (p): ");
                 Scanner scanner = new Scanner(System.in);
                 db = scanner.nextLine();
-                if ("o".equals(db)) {
-                    driver = new oracle.jdbc.OracleDriver();
+                boolean success = setDriverByDbType(db);
+                if (success) {
                     break;
-                } else if ("m".equals(db)) {
-                    driver = new com.mysql.cj.jdbc.Driver();
-                    break;
-                } else if ("p".equals(db)) {
-                    driver = new org.postgresql.Driver();
-                    break;
-                } else {
-                    System.out.println("Type Error");
                 }
             }
 
-            System.out.print("[Short Jdbc Url ( eg. localhost:1522/dbname OR 192.168.0.202:1521:xe )]: ");
+            System.out.print("[Jdbc Url (Also Can short style~ localhost:1522/dbname OR 192.168.0.202:1521:xe )]: ");
             Scanner scanner = new Scanner(System.in);
             String simpleUrl = scanner.next();
-            if ("o".equals(db)) {
-                url = "jdbc:oracle:thin:@{url}".replace("{url}", simpleUrl);
-            } else if ("m".equals(db)) {
-                url = "jdbc:mysql://{url}?useSsl=false".replace("{url}", simpleUrl);
-            } else if ("p".equals(db)) {
-                url = "jdbc:postgresql://{url}?stringtype=unspecified".replace("{url}", simpleUrl);
-            }
+            setUrl(simpleUrl);
 
             System.out.print("[Username] : ");
             userName = new Scanner(System.in).nextLine().trim();
 
             System.out.print("[Password] : ");
             password = new Scanner(System.in).nextLine().trim();
-        } else {
-            System.out.println("Mode Error");
-            System.exit(0);
         }
 
 
@@ -84,6 +85,7 @@ public class MainApp {
         System.out.println("===========SUCCESS=========");
         System.out.println("[INFO] " + url + "  ");
         while (true) {
+            System.out.println();
             System.out.println("[Please input sql] : ");
             String sql = new Scanner(System.in).nextLine();
             if (sql.equals("exit")) {
@@ -103,11 +105,58 @@ public class MainApp {
         }
     }
 
-    static void printMaps(List<Map<String, Object>> maps) {
+    private static void printMaps(List<Map<String, Object>> maps) {
         int i = 0;
         for (Map<String, Object> map : maps) {
             i++;
             System.out.println("-> " + i + " " + map);
+        }
+    }
+
+    /**
+     * 根据类型设置Driver
+     */
+    private static boolean setDriverByDbType(String db) throws SQLException {
+        if ("o".equals(db) || "oracle".equals(db)) {
+            driver = new oracle.jdbc.OracleDriver();
+            dbType = DbType.ORACLE;
+            return true;
+        } else if ("m".equals(db) || "mysql".equals(db)) {
+            driver = new com.mysql.cj.jdbc.Driver();
+            dbType = DbType.MY_SQL;
+            return true;
+        } else if ("p".equals(db) || "postgresql".equals(db)) {
+            driver = new org.postgresql.Driver();
+            dbType = DbType.POSTGRESQL;
+            return true;
+        } else {
+            System.out.println("Db Type Error :" + db);
+            return false;
+        }
+    }
+
+    private static void setUrl(String inUrl) throws SQLException {
+
+        if (dbType.equals(DbType.ORACLE)) {
+            if (inUrl.contains("jdbc:")) {
+                url = inUrl;
+            } else {
+                url = "jdbc:oracle:thin:@{url}".replace("{url}", inUrl);
+            }
+
+        } else if (dbType.equals(DbType.MY_SQL)) {
+            if (inUrl.contains("jdbc:")) {
+                url = inUrl;
+            } else {
+                url = "jdbc:mysql://{url}?useSsl=false".replace("{url}", inUrl);
+            }
+
+        } else if (dbType.equals(DbType.POSTGRESQL)) {
+            if (inUrl.contains("jdbc:")) {
+                url = inUrl;
+            } else {
+                url = "jdbc:postgresql://{url}?stringtype=unspecified".replace("{url}", inUrl);
+            }
         }
     }
 }
